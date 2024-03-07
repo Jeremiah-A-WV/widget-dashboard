6c94c433352fe81b0f61275deac46cfcb5a915cb
Jeremiah Whitehurst <jalexander-wv@protonmail.com>
2024-03-07 18:55:29 -0500

    added numbers for analog clock

--- dashboard/src/components/AnalogClock.js
+++ dashboard/src/components/AnalogClock.js
@@ -6,6 +6,19 @@ const AnalogClock = () => {
     return (
 
         <div className="widget--clock__analog">
+            <div className="middle"></div>
+            <div className="analog__hour analog__hour--1">1</div>
+            <div className="analog__hour analog__hour--2">2</div>
+            <div className="analog__hour analog__hour--3">3</div>
+            <div className="analog__hour analog__hour--4">4</div>
+            <div className="analog__hour analog__hour--5">5</div>
+            <div className="analog__hour analog__hour--6">6</div>
+            <div className="analog__hour analog__hour--7">7</div>
+            <div className="analog__hour analog__hour--8">8</div>
+            <div className="analog__hour analog__hour--9">9</div>
+            <div className="analog__hour analog__hour--10">10</div>
+            <div className="analog__hour analog__hour--11">11</div>
+            <div className="analog__hour analog__hour--12">12</div>
         </div>
 
     );
