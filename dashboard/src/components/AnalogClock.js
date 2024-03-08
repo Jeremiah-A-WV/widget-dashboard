e40f8fbc8cd581afd2e4f25c72ea884bbfb76539
Jeremiah Whitehurst <jalexander-wv@protonmail.com>
2024-03-08 15:20:29 -0500

    added analog clock hands

--- dashboard/src/components/AnalogClock.js
+++ dashboard/src/components/AnalogClock.js
@@ -7,18 +7,9 @@ const AnalogClock = () => {
 
         <div className="widget--clock__analog">
             <div className="middle"></div>
-            <div className="analog__hour analog__hour--1">1</div>
-            <div className="analog__hour analog__hour--2">2</div>
-            <div className="analog__hour analog__hour--3">3</div>
-            <div className="analog__hour analog__hour--4">4</div>
-            <div className="analog__hour analog__hour--5">5</div>
-            <div className="analog__hour analog__hour--6">6</div>
-            <div className="analog__hour analog__hour--7">7</div>
-            <div className="analog__hour analog__hour--8">8</div>
-            <div className="analog__hour analog__hour--9">9</div>
-            <div className="analog__hour analog__hour--10">10</div>
-            <div className="analog__hour analog__hour--11">11</div>
-            <div className="analog__hour analog__hour--12">12</div>
+            <div className="analog__hand--minute"></div>
+            <div className="analog__hand--hour"></div>
+            <div className="analog__hand--second"></div>
         </div>
 
     );
